3fdd9aa027c7583d1bbde664dc6d2e78b6288917
Roman Akash <romanakash2000@gmail.com>
2019-02-21 20:33:49 +0000

    second

---
+++ receiver.py
@@ -0,0 +1,29 @@
+import socket
+import struct
+from network import LoRa
+
+# package config varibales
+# struct format for data which we send
+_LORA_PKG_FORMAT = "BB%ds"
+# receiving data format
+_LORA_PKG_ACK_FORMAT = "BBB"
+STATUS_CODE_GOOD = 250
+
+# config for LORA network
+# use rx_iq to avoid listening to our own messages
+lora = LoRa(mode=LoRa.LORA, rx_iq=True, region=LoRa.EU868)
+lora_sock = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+
+lora_sock.setblocking(False)
+
+while (True):
+   recv_pkg = lora_sock.recv(512)
+   # check if data received has any message
+   if (len(recv_pkg) > 2):
+       recv_pkg_len = recv_pkg[1]
+       # unpack to regular python strings
+       device_id, pkg_len, msg = struct.unpack(_LORA_PKG_FORMAT % recv_pkg_len, recv_pkg)
+       print('Device: %d - Pkg:  %s' % (device_id, msg))
+       # pack to send back data was received
+       ack_pkg = struct.pack(_LORA_PKG_ACK_FORMAT, device_id, 1, STATUS_CODE_GOOD)
+       lora_sock.send(ack_pkg)
